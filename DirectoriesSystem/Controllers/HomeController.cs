5432614bd463c9425cb82f8cea7d0312b573ff47
bstaykov <boycho.staykov@gmail.com>
2015-06-22 01:33:31 +0300

    Basic Display

--- DirectoriesSystem/Controllers/HomeController.cs
+++ DirectoriesSystem/Controllers/HomeController.cs
@@ -8,18 +8,26 @@
 
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        private const string RelativePath = @"\Files\RootFolder\";
+
+        public ActionResult Index(string parentDirectory)
         {
-            string relativePath = Server.MapPath(@"~\Files");
+            string relativePath = Server.MapPath("~" + RelativePath);
 
             try
             {
                 if (Directory.Exists(relativePath))
                 {
+                    string[] files;
+                    string[] directories;
+
+                    ExtractPaths(relativePath, out files, out directories);
+
                     var browseViewModel = new BrowseViewModel()
                     {
-                        Files = Directory.GetFiles(relativePath),
-                        Directories = Directory.GetDirectories(relativePath),
+                        Files = files,
+                        Directories = directories,
+                        ParentDirectory = parentDirectory,
                     };
 
                     return this.View(browseViewModel);
@@ -31,6 +39,25 @@
             }
 
             return this.View();
+        }
+
+        private void ExtractPaths(string relativePath, out string[] files, out string[] directories)
+        {
+            string[] fullFilesPaths = Directory.GetFiles(relativePath);
+            string[] fullDirectoriesPaths = Directory.GetDirectories(relativePath);
+
+            files = new string[fullFilesPaths.Length];
+            directories = new string[fullDirectoriesPaths.Length];
+
+            for (int i = 0; i < files.Length; i++)
+            {
+                files[i] = fullFilesPaths[i].Substring(fullFilesPaths[i].IndexOf(RelativePath) + RelativePath.Length);
+            }
+
+            for (int i = 0; i < directories.Length; i++)
+            {
+                directories[i] = fullDirectoriesPaths[i].Substring(fullDirectoriesPaths[i].IndexOf(RelativePath) + RelativePath.Length);
+            }
         }
     }
 }
